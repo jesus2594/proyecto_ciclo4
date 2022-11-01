763310aa51c5fbac359119b6b44e8d8245af5ef5
jesus2594 <jesusdavid890@hotmail.com>
2022-11-01 00:50:41 -0500

    jsonwebtoken agregado

--- proyecto_back/app.js
+++ proyecto_back/app.js
@@ -4,10 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var database = require("./config/database")
+var auth = require("./auth/main_auth")
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 var productosRouter = require('./routes/productos.router');
+var usuariosRouter = require('./routes/usuario.router');
 
 var app = express();
 
@@ -21,9 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Mongo connection
 database.mongoConnect();
 
+app.use('/usuarios', usuariosRouter);
+app.use(auth)
+
 //Router
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/productos', productosRouter);
 
 // catch 404 and forward to error handler
